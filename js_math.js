e9aa5d704983f1dd040426508704a4a34b660d76
LaManshe <ilm9816@gmail.com>
2022-07-24 02:58:08 +0500

    Intersections

--- js_math.js
+++ js_math.js
@@ -54,5 +54,42 @@ export default class Math{
         return result;
     }
 
-    
+    Intersections(isLogging){
+        var _logging = false;
+        if(typeof(isLogging) == "boolean"){
+            _logging = isLogging;
+        }
+
+        var result = new class Intersections{
+            constructor(){
+                this.logging = _logging;
+            }
+
+            isUserSee(target){
+                var targetPosition = {
+                    top: window.pageYOffset + target.getBoundingClientRect().top,
+                    left: window.pageXOffset + target.getBoundingClientRect().left,
+                    right: window.pageXOffset + target.getBoundingClientRect().right,
+                    bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+                },
+                windowPosition = {
+                    top: window.pageYOffset,
+                    left: window.pageXOffset,
+                    right: window.pageXOffset + document.documentElement.clientWidth,
+                    bottom: window.pageYOffset + document.documentElement.clientHeight
+                };
+              
+                if (targetPosition.bottom > windowPosition.top && 
+                  targetPosition.top < windowPosition.bottom && 
+                  targetPosition.right > windowPosition.left && 
+                  targetPosition.left < windowPosition.right){ 
+                    return true;
+                } 
+                else{
+                    return false;
+                };
+            }
+        };
+        return result;
+    }
 }
